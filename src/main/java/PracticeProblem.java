576e7f2f125e4cfcdde9efaa6c873d4d979293c3
raymond-zeng-ics4u1-03-25 <raymond.zeng4@student.tdsb.on.ca>
2025-03-23 22:15:25 +0000

    update files

--- src/main/java/PracticeProblem.java
+++ src/main/java/PracticeProblem.java
@@ -3,5 +3,44 @@ public class PracticeProblem {
 	public static void main(String args[]) {
 
 	}
+	public static String pyramid(int num){
+		String drawing = "";
+		for (int i = 0; i  <  num; i++) {
+            for (int j = num-1; j  >  i; j--) {
+                drawing +=(" ");
+            }
+            for (int k = 0; k  <= i; k++) {
+                drawing +=("*");
+				if (k < i){drawing += " ";}
+            }
+            if (i<num-1){drawing +=("\n");}
+        }
+		return drawing;
+    }
 
+	public static String square(int num){
+		String drawing = "";
+		for (int i = 0; i < num; i++){
+			if (i == 0){
+				for (int j = 0;j < num;j++){
+					drawing += "*";}}
+			else if (i == num-1){
+				for (int j = 0;j < num;j++){
+						drawing += "*";}}
+			else {drawing += "*";
+				for (int k = 2;k < num;k++){
+				drawing += " ";
+				if (k == num-1){drawing += "*";}}}
+			if (i < num - 1){drawing += "\n";}}
+		return drawing;
+			}	
+		public static boolean hasLowercase(String word){
+			for (int i = 0; i < word.length(); i++) {
+				if (Character.isLowerCase(word.charAt(i))) {
+					return true; 
+				}
+			}
+			return false;
+	}
 }
+
